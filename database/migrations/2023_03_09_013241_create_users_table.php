ea886b4c5042e5f85c10a3982b70e1510ddd7cef
Guilherme Silva <guilhermeccb222@hotmail.com>
2023-03-08 23:20:11 -0300

    Criando uma migration user

---
+++ database/migrations/2023_03_09_013241_create_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100);
+            $table->string('phone', 11);
+            $table->string('cpf', 11);
+            $table->string('endereco');
+            $table->string('email', 70);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+};
